e8c4603210a8e00b1a6d5373510a74c9bcc5b527
Sergey Butyanov <igor@tomatstudio.ru>
2020-03-23 13:39:51 +0300

    Item page + slick

--- js/main.js
+++ js/main.js
@@ -11,3 +11,36 @@ $('.filterBtn').click(function(e){
 
   $('.filterHidden').slideToggle();
 });
+
+
+//Tabs
+$(".tabBody").hide();
+$(".tabBody:first").show();    
+  
+$(".tabBtn").click(function() {
+  $(".tabBody").hide();
+  let activeTab = $(this).attr("rel");
+  $("#"+activeTab).fadeIn();
+      
+  $(".tabBtn").removeClass("active");
+  $(this).addClass("active");
+});
+
+
+$('.photos-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  adaptiveHeight: true,
+  asNavFor: '.photos-nav'
+});
+
+$('.photos-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.photos-for',
+  dots: false,
+  centerMode: false,
+  focusOnSelect: true
+});
